d5efebb2b7ebc043a59d3af8d2627624aac8e230
denjayx <denyw602@gmail.com>
2023-10-21 11:31:19 +0700

    request from API

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'f8867f28bd1a9983653cc52d732b96b7',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
