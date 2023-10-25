ed82dd0317fdfeb287a43c9f08a2580a27e3ae76
denjayx <denyw602@gmail.com>
2023-10-25 13:19:24 +0700

    push notification

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,9 @@ import '../styles/style.css';
 import '../styles/responsive.css';
 import App from './views/app';
 import swRegister from './utils/sw-register';
+import CONFIG from './globals/config';
+import FooterToolsInitiator from './utils/footer-tools-initiator';
+import WebSocketInitiator from './utils/websocket-initiator';
 
 const app = new App({
   button: document.querySelector('#hamburgerButton'),
@@ -17,4 +20,10 @@ window.addEventListener('hashchange', () => {
 window.addEventListener('load', () => {
   app.renderPage();
   swRegister();
+  WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
+
+  FooterToolsInitiator.init({
+    subscribeButton: document.querySelector('#subscribePushNotification'),
+    unsubscribeButton: document.querySelector('#unsubscribePushNotification'),
+  });
 });
